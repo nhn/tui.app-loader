1481cb5687ed24df0d49f0c0e3c44ab0a54bfae0
jein.yi <ahin1127>
2015-03-27 14:17:41 +0900

    file upload

---
+++ js/code-snippet.js
@@ -0,0 +1,657 @@
+/**
+ * @fileoverview
+ * @author FE개발팀
+ */
+
+(function(ne) {
+    'use strict';
+    /* istanbul ignore if */
+    if (!ne) {
+        ne = window.ne = {};
+    }
+    if (!ne.util) {
+        ne.util = window.ne.util = {};
+    }
+
+    /**
+     * 데이터 객체를 확장하는 메서드 (deep copy 는 하지 않는다)
+     * @param {object} target - 확장될 객체
+     * @param {...object} objects - 프로퍼티를 복사할 객체들
+     * @return {object}
+     * @memberOf ne.util
+     */
+    function extend(target, objects) {
+        var source,
+            prop,
+            hasOwnProp = Object.prototype.hasOwnProperty,
+            i,
+            len;
+
+        for (i = 1, len = arguments.length; i < len; i++) {
+            source = arguments[i];
+            for (prop in source) {
+                if (hasOwnProp.call(source, prop)) {
+                    target[prop] = source[prop];
+                }
+            }
+        }
+        return target;
+    }
+
+    /**
+     * @type {number}
+     */
+    var lastId = 0;
+
+    /**
+     * 객체에 unique한 ID를 프로퍼티로 할당한다.
+     * @param {object} obj - ID를 할당할 객체
+     * @return {number}
+     * @memberOf ne.util
+     */
+    function stamp(obj) {
+        obj.__fe_id = obj.__fe_id || ++lastId;
+        return obj.__fe_id;
+    }
+
+    /**
+     * object#stamp로 UniqueID를 부여했었는지 여부 확인
+     * @param {object} obj
+     * @returns {boolean}
+     * @memberOf ne.util
+     */
+    function hasStamp(obj) {
+        return ne.util.isExisty(obj, '__fe_id');
+    }
+
+    function resetLastId() {
+        lastId = 0;
+    }
+
+    /**
+     * 객체를 전달받아 객체의 키목록을 배열로만들어 리턴해준다.
+     * @param obj
+     * @returns {Array}
+     * @memberOf ne.util
+     */
+    var keys = function(obj) {
+        var keys = [],
+            key;
+
+        for (key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                keys.push(key);
+            }
+        }
+
+        return keys;
+    };
+
+
+    /**
+     *
+     * 여러개의 json객체들을 대상으로 그것들이 동일한지 비교하여 리턴한다.
+     * (출처) http://stackoverflow.com/questions/1068834/object-comparison-in-javascript
+     *
+     * @param {...object} object 비교할 객체 목록
+     * @return {boolean} 파라미터로 전달받은 json객체들의 동일 여부
+     * @example
+     *
+     var jsonObj1 = {name:'milk', price: 1000},
+     jsonObj2 = {name:'milk', price: 1000},
+     jsonObj3 = {name:'milk', price: 1000}
+
+     ne.util.compareJSON(jsonObj1, jsonObj2, jsonObj3);
+     => return true
+
+     var jsonObj4 = {name:'milk', price: 1000},
+     jsonObj5 = {name:'beer', price: 3000}
+
+     ne.util.compareJSON(jsonObj4, jsonObj5);
+     => return false
+
+     * @memberOf ne.util
+     */
+    function compareJSON(object) {
+        var leftChain,
+            rightChain,
+            argsLen = arguments.length,
+            i;
+
+        function isSameObject(x, y) {
+            var p;
+
+            // remember that NaN === NaN returns false
+            // and isNaN(undefined) returns true
+            if (isNaN(x) &&
+                isNaN(y) &&
+                ne.util.isNumber(x) &&
+                ne.util.isNumber(y)) {
+                return true;
+            }
+
+            // Compare primitives and functions.
+            // Check if both arguments link to the same object.
+            // Especially useful on step when comparing prototypes
+            if (x === y) {
+                return true;
+            }
+
+            // Works in case when functions are created in constructor.
+            // Comparing dates is a common scenario. Another built-ins?
+            // We can even handle functions passed across iframes
+            if ((ne.util.isFunction(x) && ne.util.isFunction(y)) ||
+                (x instanceof Date && y instanceof Date) ||
+                (x instanceof RegExp && y instanceof RegExp) ||
+                (x instanceof String && y instanceof String) ||
+                (x instanceof Number && y instanceof Number)) {
+                return x.toString() === y.toString();
+            }
+
+            // At last checking prototypes as good a we can
+            if (!(x instanceof Object && y instanceof Object)) {
+                return false;
+            }
+
+            if (x.isPrototypeOf(y) ||
+                y.isPrototypeOf(x) ||
+                x.constructor !== y.constructor ||
+                x.prototype !== y.prototype) {
+                return false;
+            }
+
+            // check for infinitive linking loops
+            if (ne.util.inArray(x, leftChain) > -1 ||
+                ne.util.inArray(y, rightChain) > -1) {
+                return false;
+            }
+
+            // Quick checking of one object beeing a subset of another.
+            for (p in y) {
+                if (y.hasOwnProperty(p) !== x.hasOwnProperty(p)) {
+                    return false;
+                }
+                else if (typeof y[p] !== typeof x[p]) {
+                    return false;
+                }
+            }
+
+            //인풋 데이터 x의 오브젝트 키값으로 값을 순회하면서
+            //hasOwnProperty, typeof 체크를 해서 비교하고 x[prop]값과 y[prop] 가 같은 객체인지 판별한다.
+            for (p in x) {
+                if (y.hasOwnProperty(p) !== x.hasOwnProperty(p)) {
+                    return false;
+                }
+                else if (typeof y[p] !== typeof x[p]) {
+                    return false;
+                }
+
+                if (typeof(x[p]) === 'object' || typeof(x[p]) === 'function') {
+                    leftChain.push(x);
+                    rightChain.push(y);
+
+                    if (!isSameObject(x[p], y[p])) {
+                        return false;
+                    }
+
+                    leftChain.pop();
+                    rightChain.pop();
+                } else if (x[p] !== y[p]) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        if (argsLen < 1) {
+            return true;
+        }
+
+        for (i = 1; i < argsLen; i++) {
+            leftChain = [];
+            rightChain = [];
+
+            if (!isSameObject(arguments[0], arguments[i])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    ne.util.extend = extend;
+    ne.util.stamp = stamp;
+    ne.util.hasStamp = hasStamp;
+    ne.util._resetLastId = resetLastId;
+    ne.util.keys = Object.keys || keys;
+    ne.util.compareJSON = compareJSON;
+})(window.ne);
+/**
+ * @fileoverview 간단한 상속 시뮬레이션
+ * @author FE개발팀
+ */
+
+(function(ne) {
+    'use strict';
+    /* istanbul ignore if */
+    if (!ne) {
+        ne = window.ne = {};
+    }
+    /* istanbul ignore if */
+    if (!ne.util) {
+        ne.util = window.ne.util = {};
+    }
+
+
+
+    /**
+     * 전달된 객체를 prototype으로 사용하는 객체를 만들어 반환하는 메서드
+     * @param {Object} obj
+     * @return {Object}
+     * @memberof ne.util
+     */
+    function createObject() {
+        function F() {}
+
+        return function(obj) {
+            F.prototype = obj;
+            return new F();
+        };
+    }
+
+    /**
+     * 단순 prototype 확장을 응용한 상속 메서드
+     *
+     * **주의점**
+     *
+     * 단순 프로토타입 확장 기능만 제공하므로 자식 생성자의 prototype을 덮어쓰면 안된다.
+     *
+     * @example
+     * function Animal(leg) {
+     *     this.leg = leg;
+     * }
+     *
+     * Animal.prototype.growl = function() {
+     *     // ...
+     * };
+     *
+     * function Person(name) {
+     *     this.name = name;
+     * }
+     *
+     * // 상속
+     * core.inherit(Person, Animal);
+     *
+     * // 이 이후부터는 프로퍼티 편집만으로 확장해야 한다.
+     * Person.prototype.walk = function(direction) {
+     *     // ...
+     * };
+     * @param {function} subType 자식 생성자 함수
+     * @param {function} superType 부모 생성자 함수
+     * @memberof ne.util
+     */
+    function inherit(subType, superType) {
+        var prototype = ne.util.createObject(superType.prototype);
+        prototype.constructor = subType;
+        subType.prototype = prototype;
+    }
+
+    ne.util.createObject = createObject();
+    ne.util.inherit = inherit;
+
+})(window.ne);
+/**
+ * @fileoverview 클래스와 비슷한방식으로 생성자를 만들고 상속을 구현할 수 있는 메소드를 제공하는 모듈
+ * @author FE개발팀
+ * @dependency inheritance.js, object.js
+ */
+
+(function(ne) {
+    'use strict';
+    /* istanbul ignore if */
+    if (!ne) {
+        ne = window.ne = {};
+    }
+    /* istanbul ignore if */
+    if (!ne.util) {
+        ne.util = window.ne.util = {};
+    }
+
+    /**
+     * 객체의 생성및 상속을 편하게 도와주는 메소드
+     * @param {*} [parent] 상속받을 생성자.
+     * @param {Object} props 생성할 생성자의프로토타입에 들어갈 멤버들
+     * @param {Function} props.init 인스턴스가 생성될때 실행됨
+     * @param {Object} props.static 생성자의 클래스 맴버형태로 들어갈 멤버들
+     * @returns {*}
+     * @example
+     *
+     * var Parent = defineClasss({
+     *     init: function() {
+     *         this.name = 'made by def';
+     *     },
+     *     method: function() {
+     *         //..can do something with this
+     *     },
+     *     static: {
+     *         staticMethod: function() {
+     *              //..do something
+     *         }
+     *     }
+     * });
+     *
+     * var Child = defineClass(Parent, {
+     *     method2: function() {}
+     * });
+     *
+     *
+     * Parent.staticMethod();
+     *
+     * var parentInstance = new Parent();
+     * console.log(parentInstance.name); //made by def
+     * parentInstance.staticMethod(); // Error
+     *
+     *
+     * var childInstance = new Child();
+     * childInstance.method();
+     * childInstance.method2();
+     * @memberof ne.util
+     *
+     */
+    var defineClass = function(parent, props) {
+        var obj;
+
+        if (!props) {
+            props = parent;
+            parent = null;
+        }
+
+        obj = props.init || function(){};
+
+        if(parent) {
+            ne.util.inherit(obj, parent);
+        }
+
+        if (props.hasOwnProperty('static')) {
+            ne.util.extend(obj, props.static);
+            delete props.static;
+        }
+
+        ne.util.extend(obj.prototype, props);
+
+        return obj;
+    };
+
+    ne.util.defineClass = defineClass;
+
+})(window.ne);
+
+/**
+ * @fileoverview 타입체크 모듈
+ * @author FE개발팀
+ */
+
+(function(ne) {
+    'use strict';
+    /* istanbul ignore if */
+    if (!ne) {
+        ne = window.ne = {};
+    }
+    if (!ne.util) {
+        ne.util = window.ne.util = {};
+    }
+
+    /**
+     * 값이 정의되어 있는지 확인(null과 undefined가 아니면 true를 반환한다)
+     * @param {*} obj
+     * @param {(String|Array)} [key]
+     * @returns {boolean}
+     * @example
+     *
+     * var obj = {a: {b: {c: 1}}};
+     * a 가 존재하는지 확인한다(존재함, true반환)
+     * ne.util.isExisty(a);
+     * => true;
+     * a 에 속성 b 가 존재하는지 확인한다.(존재함, true반환)
+     * ne.util.isExisty(a, 'b');
+     * => true;
+     * a 의 속성 b에 c가 존재하는지 확인한다.(존재함, true반환)
+     * ne.util.isExisty(a, 'b.c');
+     * => true;
+     * a 의 속성 b에 d가 존재하는지 확인한다.(존재하지 않음, false반환)
+     * ne.util.isExisty(a, 'b.d');
+     * => false;
+     * @memberOf ne.util
+     */
+    function isExisty(obj, key) {
+        if (arguments.length < 2) {
+            return !isNull(obj) && !isUndefined(obj);
+        }
+        if (!isObject(obj)) {
+            return false;
+        }
+
+        key = isString(key) ? key.split('.') : key;
+
+        if (!isArray(key)) {
+            return false;
+        }
+        key.unshift(obj);
+
+        var res = ne.util.reduce(key, function(acc, a) {
+            if (!acc) {
+                return;
+            }
+            return acc[a];
+        });
+        return !isNull(res) && !isUndefined(res);
+    }
+
+    /**
+     * 인자가 undefiend 인지 체크하는 메서드
+     * @param obj
+     * @returns {boolean}
+     * @memberOf ne.util
+     */
+    function isUndefined(obj) {
+        return obj === undefined;
+    }
+
+    /**
+     * 인자가 null 인지 체크하는 메서드
+     * @param {*} obj
+     * @returns {boolean}
+     * @memberOf ne.util
+     */
+    function isNull(obj) {
+        return obj === null;
+    }
+
+    /**
+     * 인자가 null, undefined, false가 아닌지 확인하는 메서드
+     * (0도 true로 간주한다)
+     *
+     * @param {*} obj
+     * @return {boolean}
+     * @memberOf ne.util
+     */
+    function isTruthy(obj) {
+        return isExisty(obj) && obj !== false;
+    }
+
+    /**
+     * 인자가 null, undefined, false인지 확인하는 메서드
+     * (truthy의 반대값)
+     * @param {*} obj
+     * @return {boolean}
+     * @memberOf ne.util
+     */
+    function isFalsy(obj) {
+        return !isTruthy(obj);
+    }
+
+
+    var toString = Object.prototype.toString;
+
+    /**
+     * 인자가 arguments 객체인지 확인
+     * @param {*} obj
+     * @return {boolean}
+     * @memberOf ne.util
+     */
+    function isArguments(obj) {
+        var result = isExisty(obj) &&
+            ((toString.call(obj) === '[object Arguments]') || !!obj.callee);
+
+        return result;
+    }
+
+    /**
+     * 인자가 배열인지 확인
+     * @param {*} obj
+     * @return {boolean}
+     * @memberOf ne.util
+     */
+    function isArray(obj) {
+        return toString.call(obj) === '[object Array]';
+    }
+
+    /**
+     * 인자가 객체인지 확인하는 메서드
+     * @param {*} obj
+     * @return {boolean}
+     * @memberOf ne.util
+     */
+    function isObject(obj) {
+        return obj === Object(obj);
+    }
+
+    /**
+     * 인자가 함수인지 확인하는 메서드
+     * @param {*} obj
+     * @return {boolean}
+     * @memberOf ne.util
+     */
+    function isFunction(obj) {
+        return toString.call(obj) === '[object Function]';
+    }
+
+    /**
+     * 인자가 숫자인지 확인하는 메서드
+     * @param {*} obj
+     * @return {boolean}
+     * @memberOf ne.util
+     */
+    function isNumber(obj) {
+        return toString.call(obj) === '[object Number]';
+    }
+
+    /**
+     * 인자가 문자열인지 확인하는 메서드
+     * @param obj
+     * @return {boolean}
+     * @memberOf ne.util
+     */
+    function isString(obj) {
+        return toString.call(obj) === '[object String]';
+    }
+
+    /**
+     * 인자가 불리언 타입인지 확인하는 메서드
+     * @param {*} obj
+     * @return {boolean}
+     * @memberOf ne.util
+     */
+    function isBoolean(obj) {
+        return toString.call(obj) === '[object Boolean]';
+    }
+
+    /**
+     * 인자가 HTML Node 인지 검사한다. (Text Node 도 포함)
+     * @param {HTMLElement} html
+     * @return {Boolean} HTMLElement 인지 여부
+     * @memberOf ne.util
+     */
+    function isHTMLNode(html) {
+        if (typeof(HTMLElement) === 'object') {
+            return (html && (html instanceof HTMLElement || !!html.nodeType));
+        }
+        return !!(html && html.nodeType);
+    }
+    /**
+     * 인자가 HTML Tag 인지 검사한다. (Text Node 제외)
+     * @param {HTMLElement} html
+     * @return {Boolean} HTMLElement 인지 여부
+     * @memberOf ne.util
+     */
+    function isHTMLTag(html) {
+        if (typeof(HTMLElement) === 'object') {
+            return (html && (html instanceof HTMLElement));
+        }
+        return !!(html && html.nodeType && html.nodeType === 1);
+    }
+    /**
+     * null, undefined 여부와 순회 가능한 객체의 순회가능 갯수가 0인지 체크한다.
+     * @param {*} obj 평가할 대상
+     * @return {boolean}
+     * @memberOf ne.util
+     */
+    function isEmpty(obj) {
+        var key,
+            hasKey = false;
+
+        if (!isExisty(obj)) {
+            return true;
+        }
+
+        if (isString(obj) && obj === '') {
+            return true;
+        }
+
+        if (isArray(obj) || isArguments(obj)) {
+            return obj.length === 0;
+        }
+
+        if (isObject(obj) && !isFunction(obj)) {
+            ne.util.forEachOwnProperties(obj, function() {
+                hasKey = true;
+                return false;
+            });
+
+            return !hasKey;
+        }
+
+        return true;
+
+    }
+
+    /**
+     * isEmpty 메서드와 반대로 동작한다.
+     * @param {*} obj 평가할 대상
+     * @return {boolean}
+     * @memberOf ne.util
+     */
+    function isNotEmpty(obj) {
+        return !isEmpty(obj);
+    }
+
+
+    ne.util.isExisty = isExisty;
+    ne.util.isUndefined = isUndefined;
+    ne.util.isNull = isNull;
+    ne.util.isTruthy = isTruthy;
+    ne.util.isFalsy = isFalsy;
+    ne.util.isArguments = isArguments;
+    ne.util.isArray = Array.isArray || isArray;
+    ne.util.isObject = isObject;
+    ne.util.isFunction = isFunction;
+    ne.util.isNumber = isNumber;
+    ne.util.isString = isString;
+    ne.util.isBoolean = isBoolean;
+    ne.util.isHTMLNode = isHTMLNode;
+    ne.util.isHTMLTag = isHTMLTag;
+    ne.util.isEmpty = isEmpty;
+    ne.util.isNotEmpty = isNotEmpty;
+
+})(window.ne);
