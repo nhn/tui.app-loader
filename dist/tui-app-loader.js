4c30c67ffe35753adadc0d2dd5190b18a85144b1
jajugoguma <jajugoguma@gmail.com>
2021-06-19 03:13:41 +0900

    2.1.6

--- dist/tui-app-loader.js
+++ dist/tui-app-loader.js
@@ -1,6 +1,6 @@
 /*!
  * TOAST UI App Loader
- * @version 2.1.5
+ * @version 2.1.6
  * @author NHN. FE Development Lab <dl_javascript@nhn.com>
  * @license MIT
  */
@@ -453,11 +453,16 @@ var defaultOptions = {
  * @param {boolean} [options.usageStatistics=true] - Let us know the hostname. If you don't want to send the hostname, please set to false.
  * @see AppLoader#exec
  * @example <caption>node, commonjs</caption>
- * var Apploader = require('tui-app-loader');
- * var appLoader = new AppLoader();
- * appLoader.exec(...);
- * @example <caption>brower, global namespace</caption>
- * var appLoader = new tui.AppLoader();
+ * // ES6
+ * import AppLoader from 'tui-app-loader'; // ES6
+ *
+ * // CommonJS
+ * const AppLoader = require('tui-app-loader'); // CommonJS
+ *
+ * // Browser
+ * const appLoader = new tui.AppLoader();
+ *
+ * const appLoader = new AppLoader();
  * appLoader.exec(...);
  */
 var AppLoader = defineClass(
@@ -577,7 +582,7 @@ var AppLoader = defineClass(
      * @param {Function} options.notFoundCallback It not found
      *
      * @example
-     * var loader = new tui.AppLoader();
+     * const loader = new AppLoader();
      * loader.exec({
      *      ios: {
      *          scheme: '<app-scheme>://', // iphone app scheme
